4e6c7c0b58b5fcaf798dc1c51d5ef6175868d3aa
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-08-20 20:54:11 -0400

    completed List model

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,13 +1,31 @@
+import uniqid from 'uniqid';
+
 export default class List {
     constructor() {
         this.items = [];
     }
 
-    additem (count, unit, ingredient) {
+    addItem(count, unit, ingredient) {
         const item = {
+            id: uniqid(),
             count,
             unit,
             ingredient
         }
+        this.items.push(item);
+        return item;
+    }
+
+    deleteItem(id) {
+        const index = this.items.findIndex(el => el.id === id); 
+        // Checks to see if the id passed in matches
+
+        this.items.splice(index, 1);
+        // [2,4,6].splice(1, 2) -> returns [4,6] , original array is [2]; 
+    }
+
+    updateCount(id, newCount) {
+        this.items.find(el => el.id === id).count = newCount;
     }
 }
+

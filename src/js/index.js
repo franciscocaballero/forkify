74b327e67387d4157460ac06f4e69257932436cd
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-08-24 22:04:52 -0400

    created the Likes controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
@@ -9,6 +10,7 @@ import {
     renderLoader,
     clearLoader
 } from './views/base';
+
 
 //GLOBAL STATE OF THE APP
 /**
@@ -159,6 +161,36 @@ elements.shopping.addEventListener('click', e => {
     }
 });
 
+//******* LIKES CONTROLLER ************
+const controlLike = () => {
+    if(!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    // User has not yet liked current recipe
+    if (!state.likes.isLiked(currentID)){
+        //Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        // Toggle the like button 
+
+        // Add like to UI list 
+            console.log(state.likes);
+        // User has Liked current recipe
+    } else {
+        //Remove like to the state
+        state.likes.deleteLike(currentID);
+        // Toggle the like button 
+
+        // Remove like to UI list 
+    }
+};
+
+
+
 //Handling recipe button clicks 
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -173,7 +205,11 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        //Add to list
         controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love')) {
+        // Likes conmtroller 
+        controlLike();
     }
     // console.log(state.recipe);
 });
